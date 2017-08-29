5dc825e262e0eb9953b79a49759c17e2c3c91499
“Rena” <renaadler1@gmail.com>
2017-08-28 22:31:01 -0500

    Started Update Method NEED TO FINISH

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,23 @@
 class StudentsController < ApplicationController
+  def update
+    @student = {
+      id: 45, 
+      first_name: "Rena", 
+      last_name: "Adler", 
+      email: "rena@email.com", 
+      phone_number: "555-555-2525",
+      short_bio: "Rena is pretty much awesome",
+      linkedin_url: "https://www.linkedin.com/in/renaadler/",
+      twitter_handle: "renarocks",
+      personal_blog_or_website_url: "github.com/renaadler",
+      online_Resume_url: "github.com/renaadler",
+      github_url: "github.com/renaadler",
+      photo: "thisismyphoto.jpg"
+    }
+  
+    @student[:first_name] = params[:form_name]
+
+    @student.name = student[:first_name]
+    render "update.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #this is ridiculous
-  #pizza time.
-
-  # my personal branch
-
+ get "/students/:id" => "students#update"
 end
