63998b4fd8ae5e79f7dd30f72337466fdc474816
Arun Madisetty <arun.madisetty@gmail.com>
2017-08-28 21:33:58 -0500

    Added Student hash and show.html.erb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,8 @@
 class StudentsController < ApplicationController
+  def show
+    @student = {
+        id: 2, first_name: "Arun", last_name: "Madisetty", email: "arun@email.com", phone_number: "8892335437", short_bio: "He cool", linkedin_url: "linkedin website", twitter_handle: "twitter", personal_blog_website_url: "some website", online_resume: "resume1", github_url: "aruns git", photo: "some image"
+      }
+    render "show.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #this is ridiculous
-  #pizza time.
-
-  # my personal branch
-
+  get "/students/:id" => "students#show"
 end
